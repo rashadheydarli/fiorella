89ba904f5729162a4951bf9dd8ef26edc24b514c
Rashad A. Heydarli <rashadahe@code.local>
2023-06-20 21:20:03 +0400

    added

---
+++ Fiorella/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Fiorella.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorella.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fiorella/Program.cs
@@ -0,0 +1,17 @@
+using Fiorella.DAL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+app.MapDefaultControllerRoute();
+app.UseStaticFiles();
+app.Run();
